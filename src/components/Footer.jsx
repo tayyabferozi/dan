2595fc0d5382e6c40dd17a7bfc090e1dceaa26b5
Tayyab Ferozi <ferozitayyab@gmail.com>
2021-12-07 22:51:28 +0500

    complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -50,8 +50,19 @@ const Footer = () => {
           <div className="col-6 col-md-3 mt-5 mt-md-0">
             <h5>Follow us</h5>
 
-            <div className="d-flex">
-              <a href="#0"></a>
+            <div className="d-flex" className="social">
+              <a href="#0">
+                <img src="./assets/images/twitter.png" alt="twitter" />
+              </a>
+              <a href="#0">
+                <img src="./assets/images/d.png" alt="facebook" />
+              </a>
+              <a href="#0">
+                <img src="./assets/images/linkedin.png" alt="facebook" />
+              </a>
+              <a href="#0">
+                <img src="./assets/images/m.png" alt="m" />
+              </a>
             </div>
           </div>
         </div>
@@ -79,6 +90,31 @@ const Footer = () => {
               <span className="divider">·</span>© 2021 Dan.com an Undeveloped BV
               subsidiary. All Rights Reserved.
             </div>
+          </div>
+          <div className="col-lg-2">
+            <button>
+              English{" "}
+              <svg
+                version="1.1"
+                id="Capa_1"
+                x="0px"
+                y="0px"
+                viewBox="0 0 34.1 34.1"
+                width="12px"
+                style={{ transform: "rotate(180deg)", marginLeft: ".5rem" }}
+              >
+                <g>
+                  <g>
+                    <path
+                      fill="#fff"
+                      class="st0"
+                      d="M34.1,9.5c0,0.5-0.2,1-0.6,1.4L18.4,26c-0.8,0.8-2,0.8-2.8,0L0.6,10.9c-0.8-0.8-0.8-2,0-2.8s2-0.8,2.8,0
+			L17,21.8L30.7,8.1c0.8-0.8,2-0.8,2.8,0C33.9,8.5,34.1,9,34.1,9.5z"
+                    />
+                  </g>
+                </g>
+              </svg>
+            </button>
           </div>
         </div>
       </div>
